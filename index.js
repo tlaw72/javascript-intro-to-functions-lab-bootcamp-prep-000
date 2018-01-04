db5e92c991df4d350bc8d37d930699f6666b7a59
Troy Lawrence <redirect@gmail.com>
2018-01-04 01:55:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string) {
   if (string === lowercase) {
     return "I can't hear you!";
   } else if (string === uppercase) {
-    return "YES INDEED!";
+    return 'YES INDEED!';
   } else if (string === "I love you, Grandma.") {
     return "I love you, too.";
   } else { return undefined;}
